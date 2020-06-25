8d160f958371dccde402fb484dd6081372b97cd1
Leila Skinner <beileiwang@gmail.com>
2020-06-25 14:56:01 -0500

    added typescript config for jest to work

--- src/Todo/Todo.tsx
+++ src/Todo/Todo.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-type TodoProps = {
-  description: string;
-};
+// type TodoProps = {
+//   description: string;
+// };
 
-class Todo extends React.Component<TodoProps> {
-  render() {
-    return <div>{this.props.description}</div>;
-  }
+// class Todo extends React.Component<TodoProps> {
+//   render() {
+//     return <div>{this.props.description}</div>;
+//   }
+// }
+
+function Todo(props: { description: string }) {
+  return <div>{props.description}</div>;
 }
 
 export default Todo;
